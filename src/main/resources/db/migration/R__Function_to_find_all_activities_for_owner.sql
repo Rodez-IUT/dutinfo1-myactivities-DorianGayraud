f8959848c33ed0b18b408f59b07da23c88a95e32
DORIAN GAYRAUD <dorian.gayraud@iut.local>
2019-04-17 14:24:15 +0200

    GIT Fix 6.3 registration management

--- src/main/resources/db/migration/R__Function_to_find_all_activities_for_owner.sql
+++ src/main/resources/db/migration/R__Function_to_find_all_activities_for_owner.sql
@@ -1,7 +1,8 @@
-CREATE OR REPLACE FUNCTION find_all_activities_for_owner(username varchar(20)) RETURNS SETOF activity AS $$
-  SELECT activity.*
-  FROM activity 
-  JOIN "user"
-  ON "user".id = owner_id
-  WHERE "user".username = find_all_activities_for_owner.username;
+
+CREATE OR REPLACE FUNCTION find_all_activities_for_owner(ownername varchar(500))
+RETURNS SETOF activity AS $$
+  select act.*
+  FROM activity act
+  JOIN "user" owner on owner_id=owner.id
+  WHERE owner.username = ownername;
 $$ LANGUAGE SQL;
--- src/main/resources/db/migration/R__Trigger_to_log_action_on_activities.sql
+++ src/main/resources/db/migration/R__Trigger_to_log_action_on_activities.sql
@@ -1,20 +1,38 @@
-DROP TRIGGER IF EXISTS action_log on activity;
+DROP TRIGGER IF EXISTS log_delete_activity on activity ;
+DROP TRIGGER IF EXISTS log_insert_registration on registration ;
+DROP TRIGGER IF EXISTS log_delete_registration on registration ;
 
-CREATE OR REPLACE FUNCTION action_log() RETURNS TRIGGER AS $$
-DECLARE
-	idN bigint;
-	
+-- fonction trigger
+CREATE OR REPLACE FUNCTION log_operation_on_entity()
+    RETURNS TRIGGER AS $$
 BEGIN
-	idN := OLD.id;
-	INSERT INTO action_log(id, action_name, entity_name, entity_id, author)
-	VALUES(nextval('id_generator'), lower(TG_OP), TG_RELNAME, idN, current_user);
-	RETURN NULL;
-
+     
+      IF (TG_OP = 'DELETE') THEN
+        INSERT INTO action_log (id, action_name, entity_name, entity_id, author, action_date)
+        values  (nextval('id_generator'), lower(TG_OP), TG_RELNAME, OLD.id, user, now());
+        RETURN NULL; -- le résultat est ignoré car il s'agit d'un trigger AFTER
+      ELSIF (TG_OP = 'INSERT') THEN
+        INSERT INTO action_log (id, action_name, entity_name, entity_id, author, action_date)
+        values  (nextval('id_generator'), lower(TG_OP), TG_RELNAME, NEW.id, user, now());
+        RETURN NULL; -- le résultat est ignoré car il s'agit d'un trigger AFTER
+    END IF;
+    RETURN NULL; -- le résultat est ignoré car il s'agit d'un trigger AFTER
+    
+    
 END;
-$$ LANGUAGE plpgsql;
+$$ language plpgsql;
 
-CREATE TRIGGER action_log
-	AFTER DELETE ON activity
-	FOR EACH ROW EXECUTE PROCEDURE action_log();
-
-	
+-- trigger
+CREATE TRIGGER log_delete_activity
+    AFTER DELETE ON activity
+    FOR EACH ROW EXECUTE PROCEDURE log_operation_on_entity();
+    
+-- trigger
+CREATE TRIGGER log_insert_registration
+    AFTER INSERT ON registration
+    FOR EACH ROW EXECUTE PROCEDURE log_operation_on_entity();
+    
+-- trigger
+CREATE TRIGGER log_delete_registration
+    AFTER DELETE ON registration
+    FOR EACH ROW EXECUTE PROCEDURE log_operation_on_entity();
